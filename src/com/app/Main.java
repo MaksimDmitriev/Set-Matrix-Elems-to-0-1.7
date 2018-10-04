ae7eb9c949533f35ac81d36974dd92b39cac9230
Maksim Dmitriev <maksim.a.dmitriev@gmail.com>
2018-10-04 13:32:05 +0300

    Implement the but mask approach

--- src/com/app/Main.java
+++ src/com/app/Main.java
@@ -3,21 +3,6 @@ package com.app;
 public class Main {
 
     public static void main(String[] args) {
-        int[][] m = {
-                {1, 22}
-        };
-        System.out.println(m[0].length); // 2
-        System.out.println(m.length); // 1
-        System.out.println(m[0][1]); // 22
 
-        Solution solution = new Solution();
-        int[][] a = {
-                {1, 0, 2, 4},
-                {3, 2, 1, 5},
-                {1, 1, 1, 1}
-
-        };
-        solution.solution(a);
-        System.out.println();
     }
 }
--- src/com/app/Solution.java
+++ src/com/app/Solution.java
@@ -1,35 +1,25 @@
 package com.app;
 
-import java.util.HashSet;
-import java.util.Set;
-
 public class Solution {
 
+    // You can assume that the input matrix has at most 32 columns and rows.
     public void solution(int[][] matrix) {
-        Set<Integer> cols = new HashSet<>();
-        Set<Integer> rows = new HashSet<>();
-
-        int rowCount = matrix.length;
-        for (int i = 0; i < rowCount; i++) {
+        long mask = 0;
+        // the right 32 bits for columns
+        // the left 32 bits for rows
+        for (int i = 0; i < matrix.length; i++) {
             for (int j = 0; j < matrix[i].length; j++) {
-                if (rows.contains(i) || cols.contains(j)) {
-                    continue;
-                }
                 if (matrix[i][j] == 0) {
-                    // set all values of row x to 0
-                    int x = 0;
-                    while (x < matrix[i].length) {
-                        matrix[i][x] = 0;
-                        x++;
-                    }
-                    // set all values of col y to 0
-                    int y = 0;
-                    while (y < rowCount) {
-                        matrix[y][j] = 0;
-                        y++;
-                    }
-                    cols.add(j);
-                    rows.add(i);
+                    mask |= (1L << i);
+                    mask |= (1L << (j + 32));
+                }
+            }
+        }
+
+        for (int i = 0; i < matrix.length; i++) {
+            for (int j = 0; j < matrix[i].length; j++) {
+                if ((mask & (1L << i)) != 0 || (mask & (1L << (j + 32))) != 0) {
+                    matrix[i][j] = 0;
                 }
             }
         }
